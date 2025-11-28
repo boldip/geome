08905865b59bbb33dc4153125a9a58e3236cd1cb
Paolo Boldi <paolo.boldi@unimi.it>
2025-11-28 10:11:16 +0100

    Aggiunta funzione che calcola la retta passante per due punti

--- geometry.go
+++ geometry.go
@@ -7,9 +7,25 @@ type Point struct {
   X, Y float64
 }
 
+type Line struct {
+  M, Q float64
+}
+
 func Distance(p1, p2 Point) float64 {
   return math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y))
 }
+
+func LineThru(p1, p2 Point) (r Line, ok bool) {
+  if p1 == p2 || p1.X == p2.X {
+    return
+  }
+  ok = true
+  r.M = (p2.Y - p1.Y) / (p2.X - p1.X)
+  r.Q = p1.Y - r.M * p1.X
+  return
+}
+
+
 
 func String(p Point) string {
   return fmt.Sprintf("PUNTO (%.3f,%.3f)", p.X, p.Y)
